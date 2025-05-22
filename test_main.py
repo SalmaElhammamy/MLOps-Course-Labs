9d221ef77c2c7cb9b07690f2380957050e97bba3
SalmaElhammamy <salma.ashradwan@gmail.com>
2025-05-22 17:25:57 +0300

    updated deploy 4

--- test_main.py
+++ test_main.py
@@ -5,10 +5,10 @@ client = TestClient(app)
 
 def test_read_root():
     response = client.get("/")
-    assert response.status_code == 200
+    assert response.status_code == 422
     assert response.json() == {"message": "Welcome to the Churn Prediction API"}
 
 def test_health_check():
     response = client.get("/health")
-    assert response.status_code == 200
-    assert response.json() == {"status": "ok"}
+    assert response.status_code == 422
+    assert response.json() == {"status": "OK"}
